442faff152851704a872ee2fa34b344aaef70ea7
Jeff Rowell <j3ff.rowell@gmail.com>
2025-10-20 12:14:51 -0600

    added packer require to init.lua. updated install script to unarchive the app image correctly

--- install.sh
+++ install.sh
@@ -4,13 +4,14 @@
 # Install dnf required packages
 ##########################################################################################################################
 sudo dnf install -y epel-release 
-sudo dnf install -y git zsh util-linux-user tar ripgrep
+sudo dnf install -y git zsh util-linux-user tar ripgrep gcc gcc-c++ make
 sudo dnf update -y
 
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage
 chmod +x nvim-linux-x86_64.appimage
 ./nvim-linux-x86_64.appimage --appimage-extract
-mv squashfs-root/usr/bin/nvim /bin/nvim
+sudo mv squashfs-root /opt/nvim
+sudo ln -s /opt/nvim/AppRun /bin/nvim
 rm -rf nvim-linux-x86_64.appimage squashfs-root
 
 
@@ -18,6 +19,7 @@ rm -rf nvim-linux-x86_64.appimage squashfs-root
 # Install ohmyzsh
 ##########################################################################################################################
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
+chsh -s $(which zsh)
 
 ##########################################################################################################################
 # Install powerlevel10k theme
@@ -39,10 +41,10 @@ sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting
 
 cp .p10k.zsh ~
 echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
-echo 'alias vi=nvim' >> ~/.zshrc
 echo 'alias vim=nvim' >> ~/.zshrc
 
-mkdir -p $HOME/.config/nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 cp -r nvim $HOME/.config/
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+
--- nvim/lua/scripts/init.lua
+++ nvim/lua/scripts/init.lua
@@ -1,1 +1,2 @@
+require("scripts.packer")
 require("scripts.remap")
